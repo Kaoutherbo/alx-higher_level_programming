357a333162053100b70732a46410170c5daf30c4
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2024-01-23 11:35:52 -0800

    square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" define a class Square"""
+
+
+class Square():
+    """ the class square """
+    def __init__(self, __size=0) -> None:
+        if type(__size) != int:
+            raise TypeError("size must be an integer")
+        elif __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
